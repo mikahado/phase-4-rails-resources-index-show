9764edb51005bdd0d7fb910be633ee3de833e3ec
Michael <mikahado@gmail.com>
2023-01-30 00:47:15 -0500

    completes lab

---
+++ app/controllers/bird_controller.rb
@@ -0,0 +1,18 @@
+class BirdController < ApplicationController
+
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+
+    def show 
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else  
+            render json: { error: "Bird not found" },
+    status: :not_found
+        end
+    end
+    
+end
